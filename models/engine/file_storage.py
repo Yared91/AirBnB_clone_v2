b3302744438e00586c29aadc77b3ade00095a433
Lillymuller <eleltaalemu@gimal.com>
2024-03-16 04:49:26 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -8,9 +15,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+		if cls is not None:
+		if isinstance(cls, str):
+			cls = eval(cls)
+		cdict = {key: val for key, val in self.__objects.items()
+			if instance(val, cls)}
+		return cdict
+
+	return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -26,25 +40,24 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+        """Deserializes the JSON file to __objects"""
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                data = json.load(f)
+                for obj_data in data.values():
+			obj_class = obj_data["__class__"]
+			del obj_data["__class__"]
+			obj_instance = eval(obj_class)(**obj_data)
+                        self.new(obj_instance)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+	    """ Delete the object from __object"""
+	    if obj is not None:
+	      key = "{}.{}".format(type(obj).__name__, obj.id)
+	    self.__objects.pop(key, None)
+		     
+    def close(self):
+        """ it reloads """
+	self.reload()
