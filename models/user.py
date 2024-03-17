c7554f1b9a615d8b57047b8f30734fddd4936c63
Lillymuller <eleltaalemu@gmail.com>
2024-03-17 04:16:06 -0700

    user

--- models/user.py
+++ models/user.py
@@ -8,23 +8,11 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user for mySql database
-        Attributes:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
-	"""
-
-    __tablename__ = "users"
+    """This class defines a user for mySql database"""
+     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user",
-		          cascade="all, delete, delete-orphan")
-    reviews = relationship("Review", backref="user",
-		           cascade="all, delete, delete-orphan")
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
